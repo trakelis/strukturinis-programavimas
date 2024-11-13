60d4b944cd30438bde01ecf975d7412d880e4c66
trakelis <t.seibutis@gmail.com>
2024-11-13 22:55:28 +0200

    uzsifravimas ir desifravimas ascii padarytas

--- TreciasPraktinis/main.cpp
+++ TreciasPraktinis/main.cpp
@@ -79,18 +79,100 @@ string Desifravimas(char tekstas[],char raktas[]) {
 
     return rezultatasString;
 }
+string UzsifravimasASCII(char tekstas[],char raktas[]) {
+    int indeksasTeksto=-1;
+    int indeksasRakto=-1;
+    int indeksasRezultato;
+    int tekstoIlgis = strlen(tekstas);
+    string raktasString = raktas;
+    while (raktasString.length() < strlen(tekstas)) {
+        raktasString.append(raktasString.begin(), raktasString.end());
+    }
+    strcpy(raktas, raktasString.c_str());
+    char rezultatas[50];
+    for(int i = 0; i < tekstoIlgis; i++) {
+
+        for(int j = 0; j < 94; j++) {
+            if(toupper(tekstas[i]) == static_cast<unsigned char>(j)) {
+                indeksasTeksto=j;
+            }
+            if(toupper(raktas[i]) == static_cast<unsigned char>(j)) {
+                indeksasRakto=j;
+            }
+            if(indeksasTeksto != -1 && indeksasRakto != -1) {
+                indeksasRezultato = (indeksasTeksto + indeksasRakto) % 94;
+                rezultatas[i] = (unsigned char)indeksasRezultato;
+                indeksasTeksto=-1;
+                indeksasRakto=-1;
+            }
+
+        }
+
+    }
+    string rezultatasString="";
+    for (int i = 0; i < tekstoIlgis; i++) {
+        rezultatasString=rezultatasString + rezultatas[i];
+    }
+
+    return rezultatasString;
+}
+string DesifravimasASCII(char tekstas[],char raktas[]) {
+    int indeksasTeksto=-1;
+    int indeksasRakto=-1;
+    int indeksasRezultato;
+    int tekstoIlgis = strlen(tekstas);
+    string raktasString = raktas;
+    while (raktasString.length() < strlen(tekstas)) {
+        raktasString.append(raktasString.begin(), raktasString.end());
+    }
+    strcpy(raktas, raktasString.c_str());
+    char rezultatas[50];
+    for(int i = 0; i < tekstoIlgis; i++) {
+
+        for(int j = 0; j < 94; j++) {
+            if(toupper(tekstas[i]) == static_cast<unsigned char>(j)) {
+                indeksasTeksto=j;
+            }
+            if(toupper(raktas[i]) == static_cast<unsigned char>(j)) {
+                indeksasRakto=j;
+            }
+            if(indeksasTeksto != -1 && indeksasRakto != -1) {
+                indeksasRezultato = (indeksasTeksto - indeksasRakto + 94) % 94;
+                rezultatas[i] = (unsigned char)indeksasRezultato;
+                indeksasTeksto=-1;
+                indeksasRakto=-1;
+            }
+
+        }
+
+    }
+    string rezultatasString="";
+    for (int i = 0; i < tekstoIlgis; i++) {
+        rezultatasString=rezultatasString + rezultatas[i];
+    }
+
+    return rezultatasString;
+}
+
+
+
 int main() {
 
 
     int pasirinkimas;
     char tekstas[50];
     char raktas[50];
-
+    string rezultatas;
     char uzsifruotasTekstas[50];
     char uzsifruotoRaktas[50];
+    string rezultasASCII;
 
+    cin >> tekstas;
+    cin >> raktas;
+    rezultatas = DesifravimasASCII(tekstas, raktas);
+    cout << rezultatas << endl;
 
-    while (pasirinkimas != 3) {
+  /*  while (pasirinkimas != 3) {
         cout <<"Pasirinkite operacija kuria norite atlikti su valiuta:"<<endl;
         cout <<"1. Sifravimas/Desifravimas naudojant abecele"<<endl;
         cout <<"2. Sifravimas/Desifravimas naudojant ASCII koduote"<<endl;
@@ -109,11 +191,15 @@ int main() {
                 cin>>tekstas;
                 cout <<"Irasykite rakta kuri norite naudoti:"<<endl;
                 cin>>raktas;
-
+                rezultatas = Uzsifravimas(tekstas, raktas);
+                cout << "Uzsifruotas tekstas: "<< rezultatas << endl;
             }else if (pasirinkti==2) {
+                cout <<"Irasykite teksta kuri norite desifruoti:"<<endl;
                 cin>>tekstas;
                 cout <<"Irasykite rakta kuri norite naudoti:"<<endl;
                 cin>>raktas;
+                rezultatas = Desifravimas(tekstas, raktas);
+                cout << "Desifruotas tekstas: "<< rezultatas << endl;
             }
         } else if (pasirinkimas==2){
             int pasirinkti;
@@ -135,8 +221,6 @@ int main() {
 
         }
     }
-
-
-*/
+    */
     return 0;
 }
